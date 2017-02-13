086396f133d597d9bdf55db7e4dc3b50df00b668
yogesh <yogesh.patki@gmail.com>
2017-02-13 22:26:43 +0530

    added buttons to shift waveforms

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,6 @@
 // Values for the Data Plot, they can also be obtained from a external file
 var sample1 =  [0, 0, 100, 100, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var sample2 =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 100, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-//var Nokia =   [20, -10, -20, -25, -40, 5, 10, 28, 30, 43, 65, 80];
 
 $(function(){
     init("#wave_first", sample1);
